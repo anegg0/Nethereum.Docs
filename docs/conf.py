e25c3c5d5751e474a90218f527f04f4362c4cca4
Gael Blanchemain <gael.blanchemain@protonmail.com>
2019-08-26 03:33:24 +0000

    removal outdated content

--- docs/conf.py
+++
@@ -1,8 +0,0 @@
-# from recommonmark.parser import CommonMarkParser
-extensions = ['recommonmark']
-source_parsers = {
-    '.md': CommonMarkParser,
-    '.workbook': CommonMarkParser
-}
-
-source_suffix = ['.workbook', '.md']
